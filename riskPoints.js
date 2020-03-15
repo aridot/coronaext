048876c007328834bb1d5bce82446974e6179cd5
swad <ariel.dotan@gmail.com>
2020-03-15 20:29:38 +0200

    change distance calc to modern js

--- riskPoints.js
+++ riskPoints.js
@@ -2,34 +2,34 @@ const riskDistanceMeters = 1000;
 const timeMarginsHours = 4;
 
 function findRiskPoints(userPositions, coronaPositions) {
-  return coronaPositions.filter(coronaPosition => {
-    return userPositions.some(userPosition => {
-      return moment(userPosition.time).isBetween(
-        moment(coronaPosition.startTime).subtract(timeMarginsHours, "hours"),
-        moment(coronaPosition.endTime).add(timeMarginsHours, "hours"),
-      ) && calculateDistance(
-        userPosition.position,
-        coronaPosition.position,
-      ) < riskDistanceMeters;
+    return coronaPositions.filter(coronaPosition => {
+        return userPositions.some(userPosition => {
+            return moment(userPosition.time).isBetween(
+                moment(coronaPosition.startTime).subtract(timeMarginsHours, "hours"),
+                moment(coronaPosition.endTime).add(timeMarginsHours, "hours"),
+            ) && calculateDistance(
+                userPosition.position,
+                coronaPosition.position,
+            ) < riskDistanceMeters;
+        });
     });
-  });
 }
 
 function calculateDistance({ lat: lat1, lon: lon1 }, { lat: lat2, lon: lon2 }) {
-  var earthRadiusKm = 6371;
+    const earthRadiusKm = 6371;
 
-  var dLat = degreesToRadians(lat2 - lat1);
-  var dLon = degreesToRadians(lon2 - lon1);
+    const dLat = degreesToRadians(lat2 - lat1);
+    const dLon = degreesToRadians(lon2 - lon1);
 
-  lat1 = degreesToRadians(lat1);
-  lat2 = degreesToRadians(lat2);
+    const lat1Rads = degreesToRadians(lat1);
+    const lat2Rads = degreesToRadians(lat2);
 
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-          Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  return earthRadiusKm * c;
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1Rads) * Math.cos(lat2Rads);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return earthRadiusKm * c;
 }
 
 function degreesToRadians(degrees) {
-  return degrees * Math.PI / 180;
+    return degrees * Math.PI / 180;
 }
